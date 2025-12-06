3c3b1edffcc1f89f06b78baef7b131752feb7243
Windows 10 <Windows 10@ANDREI>
2025-12-06 23:36:37 +0100

    added a background picture

--- BADERE - CAR RENTAL SYSTEM/Form1.Designer.vb
+++ BADERE - CAR RENTAL SYSTEM/Form1.Designer.vb
@@ -22,7 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.grpDetails = New System.Windows.Forms.GroupBox()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.lblID = New System.Windows.Forms.Label()
         Me.txtID = New System.Windows.Forms.TextBox()
         Me.lblCarModel = New System.Windows.Forms.Label()
@@ -37,206 +37,184 @@ Partial Class Form1
         Me.btnUpdate = New System.Windows.Forms.Button()
         Me.btnDelete = New System.Windows.Forms.Button()
         Me.btnClear = New System.Windows.Forms.Button()
-        Me.lblSearch = New System.Windows.Forms.Label()
-        Me.txtSearch = New System.Windows.Forms.TextBox()
-        Me.btnSearch = New System.Windows.Forms.Button()
-        Me.btnLoadAll = New System.Windows.Forms.Button()
         Me.dgvRentals = New System.Windows.Forms.DataGridView()
         Me.lblTitle = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         CType(Me.dgvRentals, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'grpDetails
-        '
-        Me.grpDetails.Location = New System.Drawing.Point(12, 53)
-        Me.grpDetails.Name = "grpDetails"
-        Me.grpDetails.Size = New System.Drawing.Size(200, 25)
-        Me.grpDetails.TabIndex = 0
-        Me.grpDetails.TabStop = False
-        Me.grpDetails.Text = "Rental Details"
         '
         'lblID
         '
         Me.lblID.AutoSize = True
-        Me.lblID.Location = New System.Drawing.Point(82, 123)
+        Me.lblID.Location = New System.Drawing.Point(96, 106)
+        Me.lblID.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblID.Name = "lblID"
-        Me.lblID.Size = New System.Drawing.Size(21, 13)
+        Me.lblID.Size = New System.Drawing.Size(23, 16)
         Me.lblID.TabIndex = 2
         Me.lblID.Text = "ID:"
         '
         'txtID
         '
-        Me.txtID.Location = New System.Drawing.Point(109, 116)
+        Me.txtID.Location = New System.Drawing.Point(132, 98)
+        Me.txtID.Margin = New System.Windows.Forms.Padding(4)
         Me.txtID.Name = "txtID"
-        Me.txtID.Size = New System.Drawing.Size(289, 20)
+        Me.txtID.Size = New System.Drawing.Size(390, 22)
         Me.txtID.TabIndex = 3
         '
         'lblCarModel
         '
         Me.lblCarModel.AutoSize = True
-        Me.lblCarModel.Location = New System.Drawing.Point(82, 149)
+        Me.lblCarModel.Location = New System.Drawing.Point(96, 138)
+        Me.lblCarModel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblCarModel.Name = "lblCarModel"
-        Me.lblCarModel.Size = New System.Drawing.Size(58, 13)
+        Me.lblCarModel.Size = New System.Drawing.Size(72, 16)
         Me.lblCarModel.TabIndex = 4
         Me.lblCarModel.Text = "Car Model:"
         '
         'txtCarModel
         '
-        Me.txtCarModel.Location = New System.Drawing.Point(146, 142)
+        Me.txtCarModel.Location = New System.Drawing.Point(182, 130)
+        Me.txtCarModel.Margin = New System.Windows.Forms.Padding(4)
         Me.txtCarModel.Name = "txtCarModel"
-        Me.txtCarModel.Size = New System.Drawing.Size(252, 20)
+        Me.txtCarModel.Size = New System.Drawing.Size(341, 22)
         Me.txtCarModel.TabIndex = 5
         '
         'lblRenter
         '
         Me.lblRenter.AutoSize = True
-        Me.lblRenter.Location = New System.Drawing.Point(82, 178)
+        Me.lblRenter.Location = New System.Drawing.Point(96, 174)
+        Me.lblRenter.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblRenter.Name = "lblRenter"
-        Me.lblRenter.Size = New System.Drawing.Size(73, 13)
+        Me.lblRenter.Size = New System.Drawing.Size(90, 16)
         Me.lblRenter.TabIndex = 6
         Me.lblRenter.Text = "Renter Name:"
         '
         'txtRenterName
         '
-        Me.txtRenterName.Location = New System.Drawing.Point(161, 171)
+        Me.txtRenterName.Location = New System.Drawing.Point(202, 165)
+        Me.txtRenterName.Margin = New System.Windows.Forms.Padding(4)
         Me.txtRenterName.Name = "txtRenterName"
-        Me.txtRenterName.Size = New System.Drawing.Size(237, 20)
+        Me.txtRenterName.Size = New System.Drawing.Size(321, 22)
         Me.txtRenterName.TabIndex = 7
         '
         'lblStart
         '
         Me.lblStart.AutoSize = True
-        Me.lblStart.Location = New System.Drawing.Point(82, 209)
+        Me.lblStart.Location = New System.Drawing.Point(96, 212)
+        Me.lblStart.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblStart.Name = "lblStart"
-        Me.lblStart.Size = New System.Drawing.Size(58, 13)
+        Me.lblStart.Size = New System.Drawing.Size(69, 16)
         Me.lblStart.TabIndex = 8
         Me.lblStart.Text = "Start Date:"
         '
         'dtpStart
         '
-        Me.dtpStart.Location = New System.Drawing.Point(161, 203)
+        Me.dtpStart.Location = New System.Drawing.Point(202, 205)
+        Me.dtpStart.Margin = New System.Windows.Forms.Padding(4)
         Me.dtpStart.Name = "dtpStart"
-        Me.dtpStart.Size = New System.Drawing.Size(237, 20)
+        Me.dtpStart.Size = New System.Drawing.Size(321, 22)
         Me.dtpStart.TabIndex = 9
         '
         'lblEnd
         '
         Me.lblEnd.AutoSize = True
-        Me.lblEnd.Location = New System.Drawing.Point(82, 239)
+        Me.lblEnd.Location = New System.Drawing.Point(96, 249)
+        Me.lblEnd.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblEnd.Name = "lblEnd"
-        Me.lblEnd.Size = New System.Drawing.Size(55, 13)
+        Me.lblEnd.Size = New System.Drawing.Size(66, 16)
         Me.lblEnd.TabIndex = 10
         Me.lblEnd.Text = "End Date:"
         '
         'dtpEnd
         '
-        Me.dtpEnd.Location = New System.Drawing.Point(161, 233)
+        Me.dtpEnd.Location = New System.Drawing.Point(202, 242)
+        Me.dtpEnd.Margin = New System.Windows.Forms.Padding(4)
         Me.dtpEnd.Name = "dtpEnd"
-        Me.dtpEnd.Size = New System.Drawing.Size(237, 20)
+        Me.dtpEnd.Size = New System.Drawing.Size(321, 22)
         Me.dtpEnd.TabIndex = 11
         '
         'btnAdd
         '
-        Me.btnAdd.Location = New System.Drawing.Point(80, 268)
+        Me.btnAdd.Location = New System.Drawing.Point(94, 285)
+        Me.btnAdd.Margin = New System.Windows.Forms.Padding(4)
         Me.btnAdd.Name = "btnAdd"
-        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.Size = New System.Drawing.Size(106, 28)
         Me.btnAdd.TabIndex = 12
         Me.btnAdd.Text = "Add"
         Me.btnAdd.UseVisualStyleBackColor = True
         '
         'btnUpdate
         '
-        Me.btnUpdate.Location = New System.Drawing.Point(161, 268)
+        Me.btnUpdate.Location = New System.Drawing.Point(202, 285)
+        Me.btnUpdate.Margin = New System.Windows.Forms.Padding(4)
         Me.btnUpdate.Name = "btnUpdate"
-        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.Size = New System.Drawing.Size(106, 28)
         Me.btnUpdate.TabIndex = 13
         Me.btnUpdate.Text = "Update"
         Me.btnUpdate.UseVisualStyleBackColor = True
         '
         'btnDelete
         '
-        Me.btnDelete.Location = New System.Drawing.Point(242, 268)
+        Me.btnDelete.Location = New System.Drawing.Point(310, 285)
+        Me.btnDelete.Margin = New System.Windows.Forms.Padding(4)
         Me.btnDelete.Name = "btnDelete"
-        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.Size = New System.Drawing.Size(106, 28)
         Me.btnDelete.TabIndex = 14
         Me.btnDelete.Text = "Delete"
         Me.btnDelete.UseVisualStyleBackColor = True
         '
         'btnClear
         '
-        Me.btnClear.Location = New System.Drawing.Point(323, 268)
+        Me.btnClear.Location = New System.Drawing.Point(418, 285)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(4)
         Me.btnClear.Name = "btnClear"
-        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.Size = New System.Drawing.Size(106, 28)
         Me.btnClear.TabIndex = 15
         Me.btnClear.Text = "Clear"
         Me.btnClear.UseVisualStyleBackColor = True
         '
-        'lblSearch
-        '
-        Me.lblSearch.AutoSize = True
-        Me.lblSearch.Location = New System.Drawing.Point(77, 314)
-        Me.lblSearch.Name = "lblSearch"
-        Me.lblSearch.Size = New System.Drawing.Size(44, 13)
-        Me.lblSearch.TabIndex = 16
-        Me.lblSearch.Text = "Search:"
-        '
-        'txtSearch
-        '
-        Me.txtSearch.Location = New System.Drawing.Point(126, 307)
-        Me.txtSearch.Name = "txtSearch"
-        Me.txtSearch.Size = New System.Drawing.Size(110, 20)
-        Me.txtSearch.TabIndex = 17
-        '
-        'btnSearch
-        '
-        Me.btnSearch.Location = New System.Drawing.Point(242, 304)
-        Me.btnSearch.Name = "btnSearch"
-        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
-        Me.btnSearch.TabIndex = 18
-        Me.btnSearch.Text = "Search"
-        Me.btnSearch.UseVisualStyleBackColor = True
-        '
-        'btnLoadAll
-        '
-        Me.btnLoadAll.Location = New System.Drawing.Point(323, 305)
-        Me.btnLoadAll.Name = "btnLoadAll"
-        Me.btnLoadAll.Size = New System.Drawing.Size(75, 23)
-        Me.btnLoadAll.TabIndex = 19
-        Me.btnLoadAll.Text = "Load All"
-        Me.btnLoadAll.UseVisualStyleBackColor = True
-        '
         'dgvRentals
         '
         Me.dgvRentals.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvRentals.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight
         Me.dgvRentals.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvRentals.Location = New System.Drawing.Point(12, 363)
+        Me.dgvRentals.Location = New System.Drawing.Point(19, 346)
+        Me.dgvRentals.Margin = New System.Windows.Forms.Padding(4)
         Me.dgvRentals.Name = "dgvRentals"
         Me.dgvRentals.ReadOnly = True
+        Me.dgvRentals.RowHeadersWidth = 51
         Me.dgvRentals.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullColumnSelect
-        Me.dgvRentals.Size = New System.Drawing.Size(436, 208)
+        Me.dgvRentals.Size = New System.Drawing.Size(581, 201)
         Me.dgvRentals.TabIndex = 20
         '
         'lblTitle
         '
         Me.lblTitle.AutoSize = True
         Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTitle.Location = New System.Drawing.Point(143, 23)
+        Me.lblTitle.Location = New System.Drawing.Point(211, 29)
+        Me.lblTitle.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblTitle.Name = "lblTitle"
-        Me.lblTitle.Size = New System.Drawing.Size(168, 16)
+        Me.lblTitle.Size = New System.Drawing.Size(204, 20)
         Me.lblTitle.TabIndex = 22
         Me.lblTitle.Text = "CAR RENTAL SYSTEM"
         '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(0, -46)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(614, 618)
+        Me.PictureBox2.TabIndex = 24
+        Me.PictureBox2.TabStop = False
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(460, 593)
+        Me.ClientSize = New System.Drawing.Size(613, 572)
         Me.Controls.Add(Me.lblTitle)
         Me.Controls.Add(Me.dgvRentals)
-        Me.Controls.Add(Me.btnLoadAll)
-        Me.Controls.Add(Me.btnSearch)
-        Me.Controls.Add(Me.txtSearch)
-        Me.Controls.Add(Me.lblSearch)
         Me.Controls.Add(Me.btnClear)
         Me.Controls.Add(Me.btnDelete)
         Me.Controls.Add(Me.btnUpdate)
@@ -251,16 +229,16 @@ Partial Class Form1
         Me.Controls.Add(Me.lblCarModel)
         Me.Controls.Add(Me.txtID)
         Me.Controls.Add(Me.lblID)
-        Me.Controls.Add(Me.grpDetails)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.dgvRentals, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents grpDetails As GroupBox
     Friend WithEvents lblID As Label
     Friend WithEvents txtID As TextBox
     Friend WithEvents lblCarModel As Label
@@ -275,10 +253,7 @@ Partial Class Form1
     Friend WithEvents btnUpdate As Button
     Friend WithEvents btnDelete As Button
     Friend WithEvents btnClear As Button
-    Friend WithEvents lblSearch As Label
-    Friend WithEvents txtSearch As TextBox
-    Friend WithEvents btnSearch As Button
-    Friend WithEvents btnLoadAll As Button
     Friend WithEvents dgvRentals As DataGridView
     Friend WithEvents lblTitle As Label
+    Friend WithEvents PictureBox2 As PictureBox
 End Class
--- BADERE - CAR RENTAL SYSTEM/Form1.vb
+++ BADERE - CAR RENTAL SYSTEM/Form1.vb
@@ -3,51 +3,50 @@
 Public Class Form1
 
     Dim conn As New MySqlConnection("server=localhost;userid=root;password=root;database=carrentals_db;")
-    Private Sub LoadData()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadRentals()
+    End Sub
+
+    'Load
+    Private Sub LoadRentals()
         Try
-            Dim cmd As New MySqlCommand("SELECT * FROM carrentals_tbl WHERE is_deleted = 0", conn)
-            Dim adapter As New MySqlDataAdapter(cmd)
-            Dim table As New DataTable()
-            adapter.Fill(table)
-            dgvRentals.DataSource = table
+            Dim sql = "SELECT * FROM rentals ORDER BY id DESC"
+            Dim da As New MySqlDataAdapter(sql, conn)
+            Dim dt As New DataTable()
+            da.Fill(dt)
+            dgvRentals.DataSource = dt
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        LoadData()
-    End Sub
-
-
+    'Clear
     Private Sub ClearFields()
-        txtID.Clear()
-        txtCarModel.Clear()
-        txtRenterName.Clear()
+        txtID.Text = ""
+        txtCarModel.Text = ""
+        txtRenterName.Text = ""
         dtpStart.Value = Date.Now
         dtpEnd.Value = Date.Now
-        txtSearch.Clear()
     End Sub
 
-
+    'Add
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         Try
-            Dim query As String =
-            "INSERT INTO carrentals_db(car_model, renter_name, start_date, end_date)
-             VALUES (@car, @renter, @startD, @endD)"
-
-            Dim cmd As New MySqlCommand(query, conn)
-            cmd.Parameters.AddWithValue("@car", txtCarModel.Text)
-            cmd.Parameters.AddWithValue("@rentername", txtRenterName.Text)
-            cmd.Parameters.AddWithValue("@startD", dtpStart.Value)
-            cmd.Parameters.AddWithValue("@endD", dtpEnd.Value)
-
             conn.Open()
+            Dim sql = "INSERT INTO rentals (car_model, renter_name, start_date, end_date) 
+                       VALUES (@car, @renter, @start, @end)"
+            Dim cmd As New MySqlCommand(sql, conn)
+
+            cmd.Parameters.AddWithValue("@car", txtCarModel.Text)
+            cmd.Parameters.AddWithValue("@renter", txtRenterName.Text)
+            cmd.Parameters.AddWithValue("@start", dtpStart.Value.Date)
+            cmd.Parameters.AddWithValue("@end", dtpEnd.Value.Date)
+
             cmd.ExecuteNonQuery()
             conn.Close()
 
-            MessageBox.Show("Record Added Successfully!")
-            LoadData()
+            MessageBox.Show("Rental added successfully!")
+            LoadRentals()
             ClearFields()
 
         Catch ex As Exception
@@ -58,28 +57,34 @@ Public Class Form1
 
     'Update
     Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
-        Try
-            Dim query As String =
-            "UPDATE carrentals_tbl SET
-                car_model=@car,
-                renter_name=@renter,
-                start_date=@startD,
-                end_date=@endD
-             WHERE id=@id"
 
-            Dim cmd As New MySqlCommand(query, conn)
+        If txtID.Text = "" Then
+            MessageBox.Show("Select a record to update.")
+            Exit Sub
+        End If
+
+        Try
+            conn.Open()
+            Dim sql = "UPDATE rentals SET 
+                       car_model=@car, 
+                       renter_name=@renter, 
+                       start_date=@start, 
+                       end_date=@end
+                       WHERE id=@id"
+
+            Dim cmd As New MySqlCommand(sql, conn)
+
             cmd.Parameters.AddWithValue("@car", txtCarModel.Text)
             cmd.Parameters.AddWithValue("@renter", txtRenterName.Text)
-            cmd.Parameters.AddWithValue("@startD", dtpStart.Value)
-            cmd.Parameters.AddWithValue("@endD", dtpEnd.Value)
+            cmd.Parameters.AddWithValue("@start", dtpStart.Value.Date)
+            cmd.Parameters.AddWithValue("@end", dtpEnd.Value.Date)
             cmd.Parameters.AddWithValue("@id", txtID.Text)
 
-            conn.Open()
             cmd.ExecuteNonQuery()
             conn.Close()
 
-            MessageBox.Show("Record Updated!")
-            LoadData()
+            MessageBox.Show("Rental updated!")
+            LoadRentals()
             ClearFields()
 
         Catch ex As Exception
@@ -88,77 +93,51 @@ Public Class Form1
         End Try
     End Sub
 
-
-    'SOFT DELETE
+    'Delete
     Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        If txtID.Text = "" Then
+            MessageBox.Show("Select a record to delete.")
+            Exit Sub
+        End If
+
+        If MessageBox.Show("Delete this record?", "Confirm", MessageBoxButtons.YesNo) = DialogResult.No Then Exit Sub
+
         Try
-            Dim cmd As New MySqlCommand(
-                "UPDATE carrentals_tbl SET is_deleted = 1 WHERE id = @id", conn)
-
-            cmd.Parameters.AddWithValue("@id", txtID.Text)
-
             conn.Open()
+            Dim sql = "DELETE FROM rentals WHERE id=@id"
+            Dim cmd As New MySqlCommand(sql, conn)
+            cmd.Parameters.AddWithValue("@id", txtID.Text)
             cmd.ExecuteNonQuery()
             conn.Close()
 
-            MessageBox.Show("Record soft deleted!")
-            LoadData()
+            MessageBox.Show("Rental deleted!")
+            LoadRentals()
             ClearFields()
 
         Catch ex As Exception
             MessageBox.Show(ex.Message)
             conn.Close()
         End Try
+
     End Sub
 
-    'Search
-    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
-        Try
-            Dim cmd As New MySqlCommand(
-                "SELECT * FROM carrentals_tbl 
-                 WHERE car_model LIKE @search AND is_deleted = 0", conn)
-
-            cmd.Parameters.AddWithValue("@search", "%" & txtSearch.Text & "%")
-
-            Dim adapter As New MySqlDataAdapter(cmd)
-            Dim table As New DataTable()
-            adapter.Fill(table)
-
-            dgvRentals.DataSource = table
-
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        End Try
-    End Sub
-
-
-    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvRentals.CellClick
-        Try
-            If e.RowIndex >= 0 Then
-                Dim row As DataGridViewRow = dgvRentals.Rows(e.RowIndex)
-
-                txtID.Text = row.Cells("id").Value.ToString()
-                txtCarModel.Text = row.Cells("car_model").Value.ToString()
-                txtRenterName.Text = row.Cells("renter_name").Value.ToString()
-                dtpStart.Value = row.Cells("start_date").Value
-                dtpEnd.Value = row.Cells("end_date").Value
-            End If
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        End Try
-    End Sub
-
-    'Clear
+    'Clear Button
     Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
         ClearFields()
     End Sub
 
-    'Load All
-    Private Sub btnLoadAll_Click(sender As Object, e As EventArgs) Handles btnLoadAll.Click
-        LoadData()
+
+    Private Sub dgvRentals_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvRentals.CellClick
+        If e.RowIndex < 0 Then Exit Sub
+
+        Dim row As DataGridViewRow = dgvRentals.Rows(e.RowIndex)
+
+        txtID.Text = row.Cells("id").Value.ToString()
+        txtCarModel.Text = row.Cells("car_model").Value.ToString()
+        txtRenterName.Text = row.Cells("renter_name").Value.ToString()
+        dtpStart.Value = Convert.ToDateTime(row.Cells("start_date").Value)
+        dtpEnd.Value = Convert.ToDateTime(row.Cells("end_date").Value)
     End Sub
-
-
-
 
 End Class
